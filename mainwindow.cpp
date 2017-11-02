ef4ec48df5cb7cb83e81a3cdf4716b14592f6fdf
whiteAlchimik <ilyapavlov1996@gmail.com>
2017-11-02 18:33:41 +0300

    feat (mainwindow.ui): add buttons in the main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->setWindowFlag(Qt::WindowMinMaxButtonsHint, false);
 }
 
 MainWindow::~MainWindow()
