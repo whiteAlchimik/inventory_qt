0e756e48943b91eb0d3cbd2a07a07aa5172de02f
whiteAlchimik <ilyapavlov1996@gmail.com>
2017-11-05 01:07:38 +0300

    feat(subject.h subject.cpp): add serialization

--- subject.cpp
+++ subject.cpp
@@ -1,5 +1,7 @@
 #include "subject.h"
 
+const QString Subject::mimeType = "data/subject";
+
 Subject::Subject(const SUBJECT_TYPE subjectType,
                  const QString &pathImage) :
     _subjectType(subjectType),
@@ -12,4 +14,19 @@ Subject::~Subject()
 QString Subject::getPathImage() const
 {
     return _pathImage;
+}
+
+QDataStream & operator<<(QDataStream &out, const Subject &subject)
+{
+    out << static_cast<qint16>(subject._subjectType) << subject._pathImage;
+    return out;
+}
+
+QDataStream & operator>>(QDataStream &in, Subject &subject)
+{
+    qint16 temp;
+    in >> temp;
+    subject._subjectType = static_cast<Subject::SUBJECT_TYPE>(temp);
+    in >> subject._pathImage;
+    return in;
 }
--- subject.h
+++ subject.h
@@ -2,12 +2,13 @@
 #define SUBJECT_H
 
 #include <QString>
+#include <QDataStream>
 
 class Subject
 {
 public:
 
-    enum SUBJECT_TYPE {APPLE};
+    enum SUBJECT_TYPE : qint16 {APPLE};
 
     explicit Subject(const SUBJECT_TYPE subjectType,
                      const QString &pathImage);
@@ -16,6 +17,10 @@ public:
 
     QString getPathImage() const;
 
+    static const QString mimeType;
+
+    friend QDataStream & operator<<(QDataStream &out, const Subject &subject);
+    friend QDataStream & operator>>(QDataStream &in, Subject &subject);
 private:
     SUBJECT_TYPE _subjectType;
     QString _pathImage;
